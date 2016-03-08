a327de4b7dcc670e5f2502a8737bcd7d315254c0
louiskun <wwsh.tk@qq.com>
2016-03-08 21:07:40 +0800

    response test

---
+++ response.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from flask import Flask,make_response,url_for
+app=Flask(__name__)
+
+@app.route('/')
+def v_index():
+	return '<a href="%s">ping</a>' % url_for('v_ping')
+
+@app.route('/ping')
+def v_ping():
+	return 'pong'
+
+if __name__ == '__main__':
+	app.run()
+
