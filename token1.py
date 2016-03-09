219451928b868ecead23176d5657869b685b89d8
louiskun <wwsh.tk@qq.com>
2016-03-09 14:01:29 +0800

    token

---
+++ token1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from flask import Flask,abort,request
+app=Flask(__name__)
+
+@app.route('/')
+def v_index():
+	return '''
+		<ul>
+		    <li><a href="/admin?token=111">允许访问</a></li>
+		    <li><a href="/admin">请求被禁止</a></li>
+		</ul>
+	'''
+
+@app.route('/admin')
+def v_admin():
+	if 'token' in request.args:
+		return 'you are a good boy!'
+	else:
+		abort(401)
+
+if __name__ == '__main__':
+	app.run()
+
