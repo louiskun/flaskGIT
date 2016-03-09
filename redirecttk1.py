6d9d36d8937da9f9384245f754178fe6da50fb0d
louiskun <wwsh.tk@qq.com>
2016-03-09 12:50:32 +0800

    cookie+redi"

--- redirecttk1.py
+++ redirecttk1.py
@@ -1,14 +1,25 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-from flask import Flask,redirect
+from flask import Flask,redirect,request,make_response
 app=Flask(__name__)
 
 @app.route('/')
 def v_index():
-	return redirect('/newbies')
+	if request.cookies != None:
+		rsp=make_response(redirect('/newbies'))
+		rsp.set_cookie('user','tongkun')
+		return rsp
+	else:
+		#如果是新用户，如何处理？
+		# rsp=make_response(redirect('/'))	
+		# rsp.set_cookie('user','tongkun')	
+		# user=request.cookies['user']
+		return 'user is news ,you are ?!' 
 
 @app.route('/newbies')
-	return 'this page is for newbies only!'
+def v_newbies():
+	user=request.cookies['user']
+	return 'this page is for newbies only!you are %s' % user
 
 if __name__ == '__main__':
 	app.run()
